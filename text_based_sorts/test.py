f1ae93f3bf47bd70dc550e085bf3a5c67ba1c606
Ashu Kher <ashukher1@gmail.com>
2020-05-02 14:25:29 -0400

    Create test.py

---
+++ text_based_sorts/test.py
@@ -0,0 +1,22 @@
+import random as rand
+import time
+
+
+def test_selection():
+    pass
+
+
+def test_insertion():
+    pass
+
+
+def test_bubble():
+    pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
