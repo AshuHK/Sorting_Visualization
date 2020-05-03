3f1d6a86eebdd1365a9a7e816f193b2a6d4fb24f
Ashu Kher <ashukher1@gmail.com>
2020-05-02 20:29:42 -0400

    Some stuff added

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Sorting_Visualization
- List sorting visualization made in Python with a GUI 
+
+List sorting visualization made in Python with a GUI 
+
+There are two folders in this repo: 
+  - text_based_sorts is for the sorts that are done with the standard Python list and contain the standard code for the sorts 
+    - Might also contain additional notes about the sorts 
+  - gui_based_sorts contains the GUI for the visualizer that will be used as a tool to help learn the sorting algorithms 
--- text_based_sorts/test.py
+++ text_based_sorts/test.py
@@ -1,22 +1,57 @@
+# import the sorts here
+
+
 import random as rand
 import time
 
 
 def test_selection():
+    """
+    """
     pass
 
 
 def test_insertion():
+    """
+    """
+
     pass
 
 
 def test_bubble():
+    """
+    """
+
+    pass
+
+
+def test_merge():
+    """
+    """
+
+    pass
+
+
+def test_quick():
+    """
+    """
+
     pass
 
 
 def main():
-    pass
+    test_selection()
+
+    test_insertion()
+
+    test_bubble()
+
+    test_merge()
+
+    test_quick()
+
+    return None
+
 
 if __name__ == "__main__":
     main()
-
