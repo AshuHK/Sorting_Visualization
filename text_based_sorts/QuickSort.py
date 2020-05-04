5247a298863f07280468a7573c4307449bdf5c70
Ashu Kher <ashukher1@gmail.com>
2020-05-04 11:24:25 -0400

    Update QuickSort.py

--- text_based_sorts/QuickSort.py
+++ text_based_sorts/QuickSort.py
@@ -3,7 +3,16 @@ from Swap import _swap
 def find_pivot(unsorted, start, end): 
     """ 
     """
-    
+    first = unsorted[start] 
+    middle = unsorted[(start + end) // 2]
+    last = unsorted[end]
+
+    if (middle <= first <= last) or (last <= first <= middle): 
+        return first 
+    elif (first <= middle <= last) or (last <= middle <= first): 
+        return middle 
+    elif (first <= last <= middle) or (middle <= last <= first): 
+        return last 
 
 def quick_sort(unsorted, start, end): 
     pass
