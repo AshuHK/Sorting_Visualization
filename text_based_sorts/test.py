8f1479bbccde97d89e8cff22878bc0ec84d288ba
Ashu Kher <ashukher1@gmail.com>
2020-05-04 09:57:36 -0400

    Update test.py

--- text_based_sorts/test.py
+++ text_based_sorts/test.py
@@ -1,12 +1,27 @@
 # import the sorts here
+from InsertionSort import insertion_sort
 
-
-import random as rand
+import random
 import time
 
 def test_insertion():
     """
     """
+    test_list = [random.randint(0,1000) for i in range(1000)] 
+
+    start_time = time.time()
+    insertion_sort(test_list, 0, len(test_list) - 1 )
+
+    # print(test_list == sorted(test_list))
+    result_string = "" 
+    if test_list == sorted(test_list): 
+        result_string += "Test: Successful\t"
+    else: 
+        result_string += "Test: Fail\t" 
+    
+    result_string += "Insertion sort time: {} seconds".format(time.time() - start_time)
+
+    print(result_string)
 
     pass
 
@@ -37,6 +52,10 @@ def test_quick():
 
 
 def main():
+
+    # just adding an empty line for readability 
+    print() 
+
     test_selection()
 
     test_insertion()
@@ -46,6 +65,8 @@ def main():
     test_merge()
 
     test_quick()
+
+    print()
 
     return None
 
