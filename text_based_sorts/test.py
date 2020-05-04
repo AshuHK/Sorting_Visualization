f72f99ccb8c223f5c7be491fd25113fec5dfee54
Ashu Kher <ashukher1@gmail.com>
2020-05-04 10:01:11 -0400

    fixed formatting

--- text_based_sorts/test.py
+++ text_based_sorts/test.py
@@ -4,32 +4,35 @@ from InsertionSort import insertion_sort
 import random
 import time
 
+
 def test_insertion():
     """
     Simple test using a random number generator to test insertion sort
     """
-    test_list = [random.randint(0,1000) for i in range(1000)] 
+    test_list = [random.randint(0, 1000) for i in range(1000)]
 
     start_time = time.time()
-    insertion_sort(test_list, 0, len(test_list) - 1 )
+    insertion_sort(test_list, 0, len(test_list) - 1)
     final_time = time.time()
 
-    result_string = "" 
-    if test_list == sorted(test_list): 
+    result_string = ""
+    if test_list == sorted(test_list):
         result_string += "Test: Successful\t"
-    else: 
-        result_string += "Test: Fail\t" 
-    
-    result_string += "Insertion sort time: {} seconds".format(final_time- start_time)
+    else:
+        result_string += "Test: Fail\t"
+
+    result_string += "Insertion sort time: {} seconds".format(final_time - start_time)
 
     print(result_string)
 
     pass
 
+
 def test_selection():
     """
     """
     pass
+
 
 def test_bubble():
     """
@@ -54,8 +57,8 @@ def test_quick():
 
 def main():
 
-    # just adding an empty line for readability 
-    print() 
+    # just adding an empty line for readability
+    print()
 
     test_selection()
 
