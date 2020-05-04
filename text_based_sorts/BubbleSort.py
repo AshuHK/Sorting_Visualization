81a7c00729ed89508217d7fe208fed42c30f8fce
Ashu Kher <ashukher1@gmail.com>
2020-05-04 10:57:19 -0400

    fixed typo that didn't pass tests

--- text_based_sorts/BubbleSort.py
+++ text_based_sorts/BubbleSort.py
@@ -17,6 +17,6 @@ def bubble_sort(unsorted):
     :param unsorted: unsorted Python list to be sorted 
     """
     for i in range(len(unsorted)): 
-        for j in range(len(unsorted) - 1, i - 1): 
+        for j in range(len(unsorted) - 1, i, -1): 
             if unsorted[j] < unsorted[j - 1]: 
                 _swap(unsorted, j, j - 1)
