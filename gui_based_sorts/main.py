931afdbf3fecb1298813b015504783e60bde9868
Ashu Kher <ashukher1@gmail.com>
2020-05-05 10:38:33 -0400

    Update main.py

--- gui_based_sorts/main.py
+++ gui_based_sorts/main.py
@@ -10,6 +10,9 @@ root.config(bg="black")
 
 selected_algorithm = StringVar()
 
+def generate(): 
+    print("Algorithm Selected: {}".format(selected_algorithm.get()))
+
 # seperating the layouts 
 ui_frame = Frame(root, width=600, height=200, bg="grey")
 ui_frame.grid(row=0, column=0, padx=10, pady=5)
@@ -24,6 +27,23 @@ Label(ui_frame, text="Algorithm: ", bg="grey").grid(row=0,column=0, padx=5, pady
 algorithm_menu = ttk.Combobox(ui_frame, textvariable=selected_algorithm, values=["Bubble sort", "Selection Sort", "Insertion sort", "Merge Sort", "Quick Sort"])
 algorithm_menu.grid(row=0, column=1, padx=5, pady=5)
 
+# set the default algorithm to be the first one in the list
 algorithm_menu.current(0)
+
+# need to figure out how to change the background color 
+Button(ui_frame, text="Generate", command=generate, bg="white").grid(row=0, column=2, padx=5, pady=5)
+
+# size input 
+Label(ui_frame, text="Size: ", bg="grey").grid(row=1,column=0, padx=5, pady=5, sticky="w")
+size_entry = Entry(ui_frame)
+size_entry.grid(row=1, column=1, padx=5, pady=5, sticky="w")
+
+Label(ui_frame, text="Min value: ", bg="grey").grid(row=1,column=2, padx=5, pady=5, sticky="w")
+min_entry = Entry(ui_frame)
+min_entry.grid(row=1, column=2, padx=5, pady=5, sticky="w")
+
+Label(ui_frame, text="Max value: ", bg="grey").grid(row=1,column=0, padx=5, pady=5, sticky="w")
+max_entry = Entry(ui_frame)
+max_entry.grid(row=1, column=3, padx=5, pady=5, sticky="w")
 
 root.mainloop()
