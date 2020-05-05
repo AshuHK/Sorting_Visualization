175aa45f894f51e7a8a2851efd9d8894d0d5aa0c
Ashu Kher <ashukher1@gmail.com>
2020-05-05 15:27:02 -0400

    Update QuickSort.py

--- gui_based_sorts/QuickSort.py
+++ gui_based_sorts/QuickSort.py
@@ -1,8 +1,17 @@
 import time 
 from Swap import _swap
 
-def find_pivot(): 
-    pass
+def find_pivot(data_list, start, end): 
+    first = data_list[start]
+    middle = data_list[(start + end) // 2] 
+    last = data_list[end] 
+
+    if (middle <= first <= last) or (last <= first <= middle): 
+        return first 
+    elif (first <= middle <= last) or (last <= middle <= first): 
+        return middle 
+    elif (first <= last <= middle) or (middle <= last <= first): 
+        return last 
 
 def partition(): 
     pass
