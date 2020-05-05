663b450663c15fd9f4986cc3c16b4795687afa4e
Ashu Kher <ashukher1@gmail.com>
2020-05-05 14:34:11 -0400

    Update InsertionSort.py

--- gui_based_sorts/InsertionSort.py
+++ gui_based_sorts/InsertionSort.py
@@ -2,4 +2,9 @@ import time
 from Swap import _swap
 
 def insertion_sort(data_list, draw_data, time_value): 
-    pass
+    for i in range(1, len(data_list)): 
+        j = i 
+
+        while (j > 0) and (data_list[j] < data_list[j - 1]): 
+            _swap(data_list, j, j - 1) 
+            j -= 1 
