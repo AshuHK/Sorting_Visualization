c65269805980b8f45a6af00455cdf05fc9426565
Ashu Kher <ashukher1@gmail.com>
2020-05-05 10:50:20 -0400

    started generation of data

--- gui_based_sorts/main.py
+++ gui_based_sorts/main.py
@@ -10,8 +10,31 @@ root.config(bg="black")
 
 selected_algorithm = StringVar()
 
+def draw_data(data_list):
+    canvas_height = 380
+    canvas_width = 600
+    x_width = canvas_width / (len(data_list) + 1) 
+
+    offset = 30
+    spacing = 10 
+    for i, height in enumerate(data_list): 
+
+        # top left 
+        x0 = i * x_width + offset + spacing
+        y0 = canvas_height - height
+        
+        # bottom right 
+        x1 = (i + 1) * x_width + offset
+        y1 = canvas_height
+
+        canvas.create_rectangle(x0, y0, x1, y1, fill="red")
+        canvas.create_text(x0+2, y0, anchor="sw", text=str(data_list[i]))
+
 def generate(): 
     print("Algorithm Selected: {}".format(selected_algorithm.get()))
+
+    data_list = [1, 2, 3, 4, 5, 100]
+    draw_data(data_list)
 
 # seperating the layouts 
 ui_frame = Frame(root, width=600, height=200, bg="grey")
@@ -40,10 +63,10 @@ size_entry.grid(row=1, column=1, padx=5, pady=5, sticky="w")
 
 Label(ui_frame, text="Min value: ", bg="grey").grid(row=1,column=2, padx=5, pady=5, sticky="w")
 min_entry = Entry(ui_frame)
-min_entry.grid(row=1, column=2, padx=5, pady=5, sticky="w")
+min_entry.grid(row=1, column=3, padx=5, pady=5, sticky="w")
 
-Label(ui_frame, text="Max value: ", bg="grey").grid(row=1,column=0, padx=5, pady=5, sticky="w")
+Label(ui_frame, text="Max value: ", bg="grey").grid(row=1,column=4, padx=5, pady=5, sticky="w")
 max_entry = Entry(ui_frame)
-max_entry.grid(row=1, column=3, padx=5, pady=5, sticky="w")
+max_entry.grid(row=1, column=5, padx=5, pady=5, sticky="w")
 
 root.mainloop()
