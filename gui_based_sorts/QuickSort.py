46f100a9e55cae533b296962ea59d69157ad8d5f
Ashu Kher <ashukher1@gmail.com>
2020-05-05 15:22:27 -0400

    Update QuickSort.py

--- gui_based_sorts/QuickSort.py
+++ gui_based_sorts/QuickSort.py
@@ -8,4 +8,13 @@ def partition():
     pass
 
 def quick_sort(data_list, start, end, draw_data, time_value): 
-    pass
+    if start >= end: 
+        return 
+
+    pivot_index = partition(data_list, start, end, draw_data, time_value) 
+
+    # sorting the first half of the data 
+    quick_sort(data_list, start, pivot_index, draw_data, time_value) 
+
+    # sorting the second half of the data 
+    quick_sort(data_list, pivot_index + 1, end, draw_data, time_value)
