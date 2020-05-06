4a63b44c228f2091861f9252f1da1d93c02d637f
Ashu Kher <ashukher1@gmail.com>
2020-05-05 21:47:33 -0400

    Update MergeSort.py

--- gui_based_sorts/MergeSort.py
+++ gui_based_sorts/MergeSort.py
@@ -1,7 +1,19 @@
 import time 
 
 def merge(data, start, end, draw_data, time_value): 
+
     pass
 
 def merge_sort(data_list, start, end, draw_data, time_value): 
-    pass
+    if start > end: 
+        return 
+
+    middle_index = (start + end) // 2 
+
+    # sort the first half of the data 
+    merge_sort(data_list, start, middle_index, draw_data, time_value) 
+
+    # sort the second half of the data 
+    merge_sort(data_list, middle_index + 1, end, draw_data, time_value) 
+    
+    merge(data_list, start, middle_index, end, draw_data, time_value)
