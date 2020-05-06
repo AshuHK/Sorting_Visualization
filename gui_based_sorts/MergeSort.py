6704e7113f0907f91358dcc88a564b54147cc5af
Ashu Kher <ashukher1@gmail.com>
2020-05-05 21:42:21 -0400

    Update MergeSort.py

--- gui_based_sorts/MergeSort.py
+++ gui_based_sorts/MergeSort.py
@@ -1,7 +1,7 @@
 import time 
 
-def merge(): 
+def merge(data, start, end, draw_data, time_value): 
     pass
 
-def merge_sort(): 
+def merge_sort(data_list, start, end, draw_data, time_value): 
     pass
