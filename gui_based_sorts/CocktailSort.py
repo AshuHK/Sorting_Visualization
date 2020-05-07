095458e4a35b812fa9da6564caca55fc4e4b95a9
Ashu Kher <ashukher1@gmail.com>
2020-05-07 19:29:37 -0400

    Create CocktailSort.py

---
+++ gui_based_sorts/CocktailSort.py
@@ -0,0 +1,31 @@
+from Swap import _swap
+
+
+def cocktail_sort(data_list, draw_data, time_value):
+    swapped_bool = True
+    start_index = 0
+    end_index = len(data_list) - 1
+
+    while swapped_bool == True:
+        swapped_bool = False
+
+        for i in range(start_index, end_index):
+            if data_list[i] > data_list[i + 1]:
+                _swap(data_list, i, i + 1)
+                swapped_bool = True
+
+        if swapped_bool == False:
+            break
+        
+        swapped_bool = False 
+        
+        end_index -= 1 
+
+        for i in range(end_index - 1, start_index - 1, -1): 
+            if data_list[i] > data_list[i + 1]: 
+                _swap(data_list, i, i + 1)
+                swapped_bool = True 
+            
+        start_index += 1 
+    
+    draw_data(data_list, ["green" for i in range(len(data_list))])
