c87d5a5e2d27cac8deb8f21eddec8d1a450393d1
Ashu Kher <ashukher1@gmail.com>
2020-05-08 13:47:37 -0400

    Update HeapSort.py

--- gui_based_sorts/HeapSort.py
+++ gui_based_sorts/HeapSort.py
@@ -19,34 +19,45 @@ def heapify(data_list, size, root_index, draw_data, time_value):
     left_index = (2 * root_index) + 1
     right_index = (2 * root_index) + 2
 
-    # 
+    # change the largest if the root is smaller than the left child
     if (left_index < size) and (data_list[root_index] < data_list[left_index]):
         largest_index = left_index
 
+    # change the largest if the largest is smaller than the right child 
     if (right_index < size) and (data_list[largest_index] < data_list[right_index]):
         largest_index = right_index
 
+    # only changes if either the left or right child is larger than the root 
     if largest_index != root_index:
         _swap(data_list, root_index, largest_index)
 
+        # generate the color list to be visualized
         color_list = ["red" for x in range(len(data_list))]
 
+        # color the two elements being swapped as blue 
         for x in range(len(color_list)):
             if (x == root_index) or (x == largest_index):
                 color_list[x] = "blue"
 
+        # visualize the step and wait for the specified amount of time 
         draw_data(data_list, color_list)
         time.sleep(time_value)
 
+        # recurse again so that it is a complete heap
         heapify(data_list, size, largest_index, draw_data, time_value)
 
 
 def heap_sort(data_list, draw_data, time_value):
+    """
+    """
 
+    # heapifies the list 
     for i in range((len(data_list) // 2) - 1, -1, -1):
         heapify(data_list, len(data_list), i, draw_data, time_value)
 
+    # draw the heapified list as blue before starting the popping from the heap
     draw_data(data_list, ["blue" for i in range(len(data_list))])
+    time.sleep(time_value)
 
     for i in range(len(data_list) - 1, 0, -1):
         _swap(data_list, i, 0)
