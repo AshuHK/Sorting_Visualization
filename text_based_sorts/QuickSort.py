edc3ab693ac9beaecbf721e0af4304d5bf341669
Ashu Kher <ashukher1@gmail.com>
2020-05-08 13:05:20 -0400

    Update QuickSort.py

--- text_based_sorts/QuickSort.py
+++ text_based_sorts/QuickSort.py
@@ -12,10 +12,13 @@ def find_pivot(unsorted, start, end):
 
     :return: the value of the pivot found using the Median of Three method
     """
+
+    # pull the first, middle, and last values in the list 
     first = unsorted[start]
     middle = unsorted[(start + end) // 2]
     last = unsorted[end]
-
+    
+    # this method is done so that it can be done in constant time
     if (middle <= first <= last) or (last <= first <= middle):
         return first
     elif (first <= middle <= last) or (last <= middle <= first):
@@ -39,12 +42,14 @@ def partition(unsorted, start, end):
     pivot_value = find_pivot(unsorted, start, end)
 
     i = start - 1
-
+    
+    # pu all of the values that are less than the pivot on the left side 
     for j in range(start, end + 1):
         if unsorted[j] < pivot_value:
             i += 1
             _swap(unsorted, i, j)
 
+    # moves the pivot value to the right spot after moving all of the values
     i += 1
     _swap(unsorted, i, unsorted.index(pivot_value))
 
@@ -65,6 +70,7 @@ def quick_sort(unsorted, start, end):
     if start >= end:
         return
 
+    # find the middle index of the list and partition it 
     pivot_index = partition(unsorted, start, end)
 
     # sorting the first half
