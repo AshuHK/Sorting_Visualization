0722b5a6a0d905c31b157513294ae0fbfa77d53c
Ashu Kher <ashukher1@gmail.com>
2020-05-08 13:00:14 -0400

    Update BubbleSort.py

--- text_based_sorts/BubbleSort.py
+++ text_based_sorts/BubbleSort.py
@@ -8,9 +8,9 @@ def bubble_sort(unsorted):
 
     :param unsorted: unsorted Python list to be sorted
     """
-    for i in range(len(unsorted) - 1): 
-        for j in range(0, len(unsorted) - i - 1): 
+    for i in range(len(unsorted) - 1):
+        for j in range(0, len(unsorted) - i - 1):
 
-            # bubbles up the largest value through the list 
-            if unsorted[j] > unsorted[j + 1]: 
+            # bubbles up the largest value through the list
+            if unsorted[j] > unsorted[j + 1]:
                 _swap(unsorted, j, j + 1)
