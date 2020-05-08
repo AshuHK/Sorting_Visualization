2ab3aad0b46130b9e84ef8a277597e081f25abeb
Ashu Kher <ashukher1@gmail.com>
2020-05-08 14:47:17 -0400

    Update QuickSort.py

--- gui_based_sorts/QuickSort.py
+++ gui_based_sorts/QuickSort.py
@@ -40,6 +40,8 @@ def partition(data_list, start, end, draw_data, time_value):
     :param end: Integer for the ending index in the list 
     :param draw_data: Function written in main.py that visualizes the list 
     :param time_value: Float based on the input for time between steps
+
+    :return: Integer for the index of the pivot value after the partition
     """
     pivot_value = find_pivot(data_list, start, end)
 
@@ -65,15 +67,20 @@ def partition(data_list, start, end, draw_data, time_value):
             time.sleep(time_value)
 
     i += 1
+
+    # does one last swap to move the pivot value in the right spot 
     swap_index = data_list.index(pivot_value)
     _swap(data_list, i, swap_index)
 
+    # generate the color list to be visualized 
     color_list = ["red" for x in range(len(data_list))]
 
+    # color the values being swapped green
     for x in range(len(color_list)):
         if (x == i) or (x == swap_index):
             color_list[x] = "green"
 
+    # visualizes the list and wait for the specified amount of time 
     draw_data(data_list, color_list)
     time.sleep(time_value)
 
