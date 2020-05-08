e95a6b2b7d73eedeee52245c22831ca33564462c
Ashu Kher <ashukher1@gmail.com>
2020-05-08 13:58:18 -0400

    Update InsertionSort.py

--- gui_based_sorts/InsertionSort.py
+++ gui_based_sorts/InsertionSort.py
@@ -17,16 +17,20 @@ def insertion_sort(data_list, draw_data, time_value):
         # takes one of the remaining values and inserts it back in the list 
         while (j > 0) and (data_list[j] < data_list[j - 1]):
             _swap(data_list, j, j - 1)
-            j -= 1
 
-            # 
+            # generate the color list to be visualized 
             color_list = ["red" for x in range(len(data_list))]
 
+            # color the values being swapped green
             for x in range(len(color_list)):
                 if (x == j) or (x == j - 1):
                     color_list[x] = "green"
 
+            # visualize the step and wait for the specified amount of time 
             draw_data(data_list, color_list)
             time.sleep(time_value)
 
+            j -= 1 
+
+    # color the whole list green after the sort 
     draw_data(data_list, ["green" for i in range(len(data_list))])
