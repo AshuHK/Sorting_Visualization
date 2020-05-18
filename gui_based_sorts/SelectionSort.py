e5d1d8f47e4a3067145f6016ed9c72762ee17cab
Ashu Kher <ashukher1@gmail.com>
2020-05-18 08:41:48 -0400

    Update SelectionSort.py

--- gui_based_sorts/SelectionSort.py
+++ gui_based_sorts/SelectionSort.py
@@ -19,6 +19,18 @@ def selection_sort(data_list, draw_data, time_value):
             if data_list[min_index] > data_list[j]:
                 min_index = j
 
+                # generate the color list to be visualized
+                color_list = ["red" for x in range(len(data_list))]
+
+                # color the min index blue
+                for x in range(len(data_list)):
+                    if x == min_index:
+                        color_list[x] = "blue"
+
+                # visualize the list and wait for the specified amount of time
+                draw_data(data_list, color_list)
+                time.sleep(time_value)
+
         _swap(data_list, i, min_index)
 
         # generate the color list to be visualized
